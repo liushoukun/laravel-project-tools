545d15e4cf421d38e7d9192485f72247905a1155
liushoukun <liushoukun66@gmail.com>
2022-09-16 23:08:30 +0800

    辅助方法修改

--- src/Http/Foundation/helpers.php
+++ src/Http/Foundation/helpers.php
@@ -1,14 +1,28 @@
 <?php
 
+use Liushoukun\LaravelProjectTools\Contracts\ClientInfo;
 use Liushoukun\LaravelProjectTools\Http\Requests\ClientInfoRequestAdapter;
 
-if (!function_exists('tools_client_info')) {
+if (!function_exists('tools_client_info_adapter')) {
     /**
      * @param \Illuminate\Http\Request|null $request
      * @return ClientInfoRequestAdapter
      */
-    function tools_client_info(?\Illuminate\Http\Request $request = null) : ClientInfoRequestAdapter
+    function tools_client_info_adapter(?\Illuminate\Http\Request $request = null) : ClientInfoRequestAdapter
     {
         return ClientInfoRequestAdapter::make($request);
+    }
+}
+
+
+if (!function_exists('tools_get_client_info')) {
+
+    /**
+     * @param array $parameters
+     * @return ClientInfo|null
+     */
+    function tools_get_client_info(array $parameters = []) : ?ClientInfo
+    {
+        return \Liushoukun\LaravelProjectTools\Facades\LaravelProjectTools::getClientInfo($parameters);
     }
 }
